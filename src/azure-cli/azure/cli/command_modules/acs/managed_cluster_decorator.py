9489489ad71f99d0ebf373215fb1abf2385f3bf2
Mikolaj Umanski <mik.umanski@gmail.com>
2025-08-29 22:25:52 +0000

    fix: access raw_param properly

--- src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
+++ src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
@@ -8711,7 +8711,7 @@ class AKSManagedClusterUpdateDecorator(BaseAKSManagedClusterDecorator):
         self._ensure_mc(mc)
 
         # Check if the parameter was explicitly provided
-        if "custom_ca_trust_certificates" in self.raw_param:
+        if "custom_ca_trust_certificates" in self.context.raw_param:
             ca_certs = self.context.get_custom_ca_trust_certificates()
             if mc.security_profile is None:
                 mc.security_profile = self.models.ManagedClusterSecurityProfile()  # pylint: disable=no-member
